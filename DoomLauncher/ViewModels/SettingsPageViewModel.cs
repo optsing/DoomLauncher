19e9f7e95402e0eecbc6d036fc9f9497b6e1a9b4
Anton Petrov <optsing@outlook.com>
2024-07-26 01:09:12 +0300

    Fix unpackaged, include all in packaged

--- DoomLauncher/ViewModels/SettingsPageViewModel.cs
+++ DoomLauncher/ViewModels/SettingsPageViewModel.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
 
